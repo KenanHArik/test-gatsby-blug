95183333f634eb95e19b36bdbaeb33eac09bf3c3
Kenan Arik <kenan.arik@intel.com>
2019-07-12 11:01:35 -0700

    updated markdown

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Kenan's Sample Blog`,
+    description: `Kenan's Sample Blog with Gatsby an Markdown.`,
+    author: `@KenanHArik`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
---
+++ src/markdown-pages/markdown-trial.md
@@ -0,0 +1,17 @@
+---
+title : "Trying out Markdown Blog"
+date : "2017-08-1"
+path: "/this/is/path"
+---
+
+# This is a blog written in markdown
+
+Sometimes basic text formatting is all you need for a good looking post.
+
+### This is h3
+
+** we can also go bold **
+
+<h1>This is an example hmtl block within markdown</h1>
+
+signing out --
